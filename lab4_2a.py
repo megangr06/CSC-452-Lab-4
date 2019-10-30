64cd2f16aea96f57356209f5e4b49c950c5a5177
megangr06 <megangr1997@yahoo.com>
2019-10-29 19:08:32 -0500

    #2 - Part A - Code and Output

---
+++ lab4_2a.py
@@ -0,0 +1,24 @@
+"""
+Lab 4: Q2A: Create an Amazon S3 Bucket
+"""
+import boto3
+import logging
+from botocore.exceptions import ClientError
+
+def create_bucket(bucket_name, region=None):
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name = region)
+            location = {'LocationConstraint' : region}
+            s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
+    except ClientError as e:
+            logging.error(e)
+            return False
+    return True
+    
+create_bucket('mcg033lab42abuc', region='us-west-1')
+
+
